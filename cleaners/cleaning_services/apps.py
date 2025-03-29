00caa665853627ab720337bc93c4e078087c5ae8
Housnah Nansereko <nhousnahishaq@gmail.com>
2025-03-29 16:01:56 +0300

    fixed a comma erro

--- cleaners/cleaning_services/apps.py
+++ cleaners/cleaning_services/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class CleaningServicesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cleaning_services'
+    name = 'cleaners.cleaning_services'
