f092de1269aec7bdd1da1daddeea154825715b60
Housnah Nansereko <nhousnahishaq@gmail.com>
2025-03-29 16:27:44 +0300

    fixed error

--- cleaners/cleaners/urls.py
+++ cleaners/cleaners/urls.py
@@ -21,7 +21,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("cleaners/cleaning_services.urls")),
+    path('', include("cleaners.cleaning_services.urls")),
+
    
 ]
 
